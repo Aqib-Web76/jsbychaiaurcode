23197e8e771a2bbc82c577385111f0b6202e47a6
Aqib-Web76 <117468357+Aqib-Web76@users.noreply.github.com>
2025-07-08 19:57:18 +0000

    datatypes and practice variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("Aqib Shinwari")
+const accountId = 123456
+let accountEmail = "aqibjan1122@gmail.com"
+var accountPassword = "878787"
+accountCity = "Peshawar"
+console.log(accountId)
+accountCity = "Lahore"
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+//types of datatypes
+//strings, bigint, boolean, number, undefined, null, symbol, object
