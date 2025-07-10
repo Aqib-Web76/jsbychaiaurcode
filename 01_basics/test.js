c3ab54a86f53c968f10a63e9240861b6c6295fe8
Aqib-Web76 <117468357+Aqib-Web76@users.noreply.github.com>
2025-07-10 19:15:06 +0000

    conversion of diff datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,4 @@
+/*
 const accountId = 123456
 let accountEmail = "aqibjan1122@gmail.com"
 var accountPassword = "878787"
@@ -7,4 +8,22 @@ accountCity = "Lahore"
 let accountState;
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 //types of datatypes
-//strings, bigint, boolean, number, undefined, null, symbol, object
+//strings, bigint, boolean, number, undefined, null, symbol, object */
+// const stdId = "Aqib"
+// console.log(typeof stdId);
+// let score = undefined
+// let valueInNumber = Number(stdId)
+// console.log(typeof valueInNumber);
+
+/*from binary to boolean
+ let isloggedIn = 0
+ let booleanIsloggedIn = Boolean(isloggedIn)
+ console.log(booleanIsloggedIn)*/
+ 
+ let someNumber = 66
+ let stringValue = String(someNumber)
+ console.log(stringValue);
+ console.log(typeof stringValue);
+ 
+
+
